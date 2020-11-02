90308380cb0cf434197a42ca3e31772827e80acd
Henrique Cervi Cutri <henrique.cutri@viavarejo.com.br>
2020-11-02 16:08:18 -0300

    Operadores.

--- main.js
+++ main.js
@@ -1,27 +1,3 @@
 var welcome = "Hello JavaScript - Automação Full Stack From QA Ninja"
 document.getElementById("welcome").append(welcome)
 console.log(welcome)
-
-// variável é um local na memória do computador onde você pode alocar valores
-// valores que poderão ser alterados durante a execução do software
-// por isso chamamos de variáveis
-
-// tipagem dinâmica, ou seja, atribui o tipo quando recebe o valor
-
-var number = 10;
-console.log(number);
-
-// typeof, que é responsável por trazer o tipo da variável
-
-console.log(typeof number);
-
-var name = ("Henrique");
-
-console.log(typeof name);
-
-var n1 = 10;
-var n2 = 20;
-
-n1 = "1" + 5;
-
-console.log(n1 + n2);
---
+++ operadores.js
@@ -0,0 +1,38 @@
+// os operadores vão nos permitir fazer operações, matemáticas, de comparação e lógicas
+
+// 2 + 2 => 4
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 / 2);
+
+
+// comparação
+
+// igual
+// console.log(2 == 2);
+// console.log(2 == 3);
+
+// não igual
+
+// console.log(2 != 2);
+
+// igual estrito
+// retorna verdadeiro se o valor e o tipo são iguais
+
+// console.log(2 === "2");
+
+// igual não estrito
+
+// console.log(3 !== "3");
+
+// maior que >
+// console.log(2 > 3);
+
+// menor que <
+// maior igual >=
+// console.log(2 >= 3);
+// console.log(2 >= 2);
+// console.log(2 >= 1);
+
+// menor igual <=
