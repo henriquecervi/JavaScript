6d6e9b7a2bc72cc00f673c6ae5c6769208bb28ef
Henrique Cervi Cutri <henrique.cutri@viavarejo.com.br>
2020-11-03 14:09:24 -0300

    Aprendendo Funções.

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
-var welcome = "Hello JavaScript - Automação Full Stack From QA Ninja"
-document.getElementById("welcome").append(welcome)
-console.log(welcome)
+function welcome() {
+    var welcome = "Hello JavaScript - Automação Full Stack From QA Ninja"
+    document.getElementById("welcome").append(welcome)
+    console.log(welcome)
+}
+
+welcome();
